5a14934e52a4e3441f2fbda2c525e1d45b9fc596
naotarian <naotarian19@gmail.com>
2022-01-11 11:42:41 +0000

    Cardstyle修正

--- src/Parts/ProductCard.js
+++ src/Parts/ProductCard.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
+import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
@@ -10,8 +11,9 @@ import styled from 'styled-components';
 const ProductName = styled(Typography)`
   display: -webkit-box;
   -webkit-box-orient: vertical;
-  -webkit-line-clamp: 1;
+  -webkit-line-clamp: 2;
   overflow: hidden;
+  height: 53px;
   font-size: 1.2rem!important;
 `
 const ProductIntroduction = styled(Typography)`
@@ -23,6 +25,7 @@ const ProductIntroduction = styled(Typography)`
 `
 const Price = styled(Typography)`
   height: 30px;
+  margin-top: 1rem!important;
 `
 
 function ProductCard(data) {
@@ -41,14 +44,16 @@ function ProductCard(data) {
         alt="green iguana"
       />
       <CardContent>
+      <CardActionArea href='/test'>
         <ProductName gutterBottom variant="h5" component="div">
-          {data.data.ProductName}
+                {data.data.ProductName}
         </ProductName>
+      </CardActionArea>
         <ProductIntroduction variant="body2" color="text.secondary">
           {data.data.ProductIntroduction}
         </ProductIntroduction>
         <Price>
-        {data.data.Price}円
+        {data.data.Price}円(税込)
         </Price>
       </CardContent>
     </Card>
